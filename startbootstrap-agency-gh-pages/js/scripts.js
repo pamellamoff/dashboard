27462f8415f4767f6f7475530a03047c60f75012
pamellamoff <88452990+pamellamoff@users.noreply.github.com>
2021-08-04 14:25:20 -0300

    Add files via upload

---
+++ startbootstrap-agency-gh-pages/js/scripts.js
@@ -0,0 +1,69 @@
+/*!
+ * Start Bootstrap - Agency v7.0.5 (https://startbootstrap.com/theme/agency)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+ */
+//
+// Scripts
+//
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector("#mainNav");
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove("navbar-shrink");
+    } else {
+      navbarCollapsible.classList.add("navbar-shrink");
+    }
+  };
+
+  // Shrink the navbar
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener("scroll", navbarShrink);
+
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector("#mainNav");
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNav",
+      offset: 40,
+    });
+  }
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+});
+
+// abrir modal
+function openModal() {
+  document.getElementById("modal").style.top = "0px";
+}
+//fechar modal
+function closeModal() {
+  document.getElementById("modal").style.top = "-100%";
+}
+//valida login
+const fields = document.querySelectorAll("[required]");
+console.log(fields)
+
+for (field of fields) {
+  field.addEventListener("invalid", (event) => {
+    console.log("campo inválido");
+  });
+}
